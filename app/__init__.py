d3b0dcdaf7e4ebc3f65ffa152ffb19768f733709
sankalp7654 <sankalp.saxena.sta@gmail.com>
2020-03-20 23:13:39 +0530

    rollbacked to previous commit

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,9 @@
 import os.path
 
 from flask import Flask
-from flask_login import LoginManager
 from pymongo import MongoClient
 
 from config import app_config
-
-from flask_dance.contrib.google import make_google_blueprint, google
 
 
 my_path = os.path.abspath(os.path.dirname(__file__))
@@ -24,19 +21,6 @@ def create_app(config_name):
 app = create_app(config_name="config")
 client = MongoClient()
 db = client['FYP']
-
-
-
-
-
-#os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = "1"
-GOOGLE_CLIENT_ID=os.environ.get('CLIENT_ID')
-GOOGLE_CLIENT_SECRET=os.environ.get('CLIENT_SECRET')
-google_blueprint = make_google_blueprint(client_id=GOOGLE_CLIENT_ID, client_secret=GOOGLE_CLIENT_SECRET)
-app.register_blueprint(google_blueprint, url_prefix='/login')
-login_manager = LoginManager(app)
-login_manager.init_app(app)
-
 
 
 # running the Flask App in debugging mode
--- config.py
+++ config.py
@@ -4,12 +4,6 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    # Configuration
-    GOOGLE_CLIENT_ID = os.environ.get("GOOGLE_CLIENT_ID", None)
-    GOOGLE_CLIENT_SECRET = os.environ.get("GOOGLE_CLIENT_SECRET", None)
-    GOOGLE_DISCOVERY_URL = (
-        "https://accounts.google.com/.well-known/openid-configuration"
-    )
     pass
 
 app_config = {
