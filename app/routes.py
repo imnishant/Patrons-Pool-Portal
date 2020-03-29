2d320de1e1a07ae786a5b1660d8e34594167601f
MANISH3612 <manishchoradia1@gmail.com>
2020-03-29 20:23:49 +0530

    HomePage..

--- app/routes.py
+++ app/routes.py
@@ -23,9 +23,9 @@ def login():
 
             session['username'] = username
             # session['c_type'] = result['c_type']
-            return render_template('home.html', username=username)
+            return render_template('home.html')
         return render_template('access_denied.html', error_msg="Username doesn't exist")
-    return render_template('home.html')
+    return render_template('landing.html')
 
 
 @app.route('/signup', methods=['GET', 'POST'])
@@ -63,12 +63,74 @@ def signup():
 @app.route('/logout')
 def logout():
     session.clear()
-    return render_template('index.html')
-
+    return render_template('logout.html')
 
 @app.route('/home')
 def home():
-    return "Welcome"
+    return render_template('home.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
+@app.route('/edit_basic')
+def edit_basic():
+    return render_template('edit-profile-basic.html')
+
+@app.route('/edit_work')
+def edit_work():
+    return render_template('edit-work-eductation.html')
+
+@app.route('/edit_interest')
+def edit_interest():
+    return render_template('edit-interest.html')
+
+@app.route('/edit_account')
+def edit_account():
+    return render_template('edit-account-setting.html')
+
+@app.route('/edit_password')
+def edit_password():
+    return render_template('edit-password.html')
+
+@app.route('/inbox')
+def inbox():
+    return render_template('inbox.html')
+
+@app.route('/followers')
+def followers():
+    return render_template('followers.html')
+
+@app.route('/images')
+def images():
+    return render_template('images.html')
+
+@app.route('/videos')
+def videos():
+    return render_template('videos.html')
+
+@app.route('/messages')
+def messages():
+    return render_template('messages.html')
+
+@app.route('/notifications')
+def notifications():
+    return render_template('notifications.html')
+
+
+@app.errorhandler(404)
+def not_found():
+    return render_template('access_denied.html', error_msg="Page Not Found")
+
+@app.errorhandler(400)
+def bad_request():
+    return render_template('access_denied.html', error_msg="Bad Request")
+
+
+@app.errorhandler(500)
+def server_error():
+    return render_template('access_denied.html', error_msg="Internal Server Error")
+
+
+#if __name__ == '__main__':
+#   app.run(debug=True)
