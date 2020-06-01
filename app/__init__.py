a0fb4bdf3df459c1a4cdc442abf4279cb8652ed0
sankalp7654 <sankalp.saxena.sta@gmail.com>
2020-06-01 14:45:56 +0530

    modified dependencies

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ import os.path
 
 from flask import Flask
 from pymongo import MongoClient
+from flask_pymongo import PyMongo
 from flask_mail import Mail
 
 from config import app_config
@@ -12,27 +13,23 @@ BLOB = os.path.join(my_path, 'static/BLOB')
 
 def create_app(config_name):
     app = Flask(__name__, instance_relative_config=True)
+    app.config['MONGO_URI'] = "mongodb://localhost:27017/FYP"
     app.secret_key = 'hello'
     app.config.from_object(app_config[config_name])
     app.config.from_pyfile(os.path.join(my_path, '../config.py'))
     app.config['MAIL_SERVER'] = 'localhost'
     app.config['MAIL_PORT'] = 2525
-
     return app
 
 
-# configuring the Flask app and SQLAlchemy
+# configuring the Flask app and PyMongo
 app: Flask = create_app(config_name="config")
-client = MongoClient()
-db = client['FYP']
+mongo = PyMongo(app)
+db = mongo.db
 mail = Mail(app)
 
 # The below command in the database to enable the search functionality
 db['user'].create_index([('email', 'text'), ('isSponsor', 'text'), ('posts.post_type', 'text'), ('posts.post_headline', 'text'), ('posts.base_price', 'text')])
-
-# running the Flask App in debugging mode
-#if __name__ == "__main__":
-   # app.run(debug=True)
 
 from app import models, routes
 
