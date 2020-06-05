2ea5dc1a4c5b58057d830e565495df2ab4c91ad6
sankalp7654 <sankalp.saxena.sta@gmail.com>
2020-06-05 23:49:34 +0530

    minor bug fixes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from app import app
 
 # Uncomment the below lines while creating the docker image
-# if __name__ == "__main__":
-#   app.run( host="0.0.0.0", port=9001)
+#if __name__ == "__main__":
+ #  app.run( host="0.0.0.0", port=9001)
